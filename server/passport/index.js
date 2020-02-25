a87239cb0ce3a505d887a475c86860e86fad4d04
Jason Novak <novak.jason7@gmail.com>
2020-02-25 11:52:19 -0500

    updated console.log to display first and last name when logging in user

--- server/passport/index.js
+++ server/passport/index.js
@@ -3,17 +3,15 @@ const LocalStrategy = require('./localStrategy');
 const User = require('../database/models/User');
 
 passport.serializeUser((user, done) => {
-	console.log('🌎  ==> SERIALIZE USER: ' + user.username);
-	console.log('🌎  ==> ');
+	console.log(`🌎  ==> Logging in ${user.firstName} ${user.lastName}`);
 	done(null, { _id: user._id });
 })
 
 passport.deserializeUser((id, done) => {
-	User.findOne( { _id: id }, 'username', (err, user) => {
-			console.log('🌎  ==> DESERIALIZE USER: ' + user.username);
-			console.log('🌎  ==> ');
-			done(null, user);
-		}
+	User.findOne({ _id: id }, 'username', (err, user) => {
+		console.log(`🌎  ==> Logging out...`);
+		done(null, user);
+	}
 	)
 })
 
